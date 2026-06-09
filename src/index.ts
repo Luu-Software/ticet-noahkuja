8032bfa5538fc6e7362af4b8a9c5d16f7b76e245
noahkuja <noahkuja17@gmail.com>
2026-06-09 12:03:41 -0300

    ...

--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,41 @@ DARIO       |       50%
 // COMPLETAR: Implementar la función calcularTotal que reciba el id del artista, la cantidad de entradas y un código de descuento (opcional) y devuelva el precio total a pagar en patacones.
 
 cuandoPasa('seleccionarArtista', ({ id, cantidad, codigoDescuento }) => {
+  function calcularTotal (id : string, cantidad : number, codigoDescuento: string): number {
+  
+
+  let precioNormal = 0
+  if (id === 'sabrina ') precioNormal = 1000;
+  else if (id === 'kgatlw ') precioNormal = 700;
+  else if (id === 'lali ') precioNormal = 500;
+  else if (id === 'magdalena ') precioNormal = 600;
+  else  if (id === 'viagra ') precioNormal = 400;
+  else if (id === 'dillom ') precioNormal = 350;
+  else if (id === 'marilina ') precioNormal = 200;
+  else if (id === 'mugre ') precioNormal = 150;
+  
+  let precio  = precioNormal * cantidad;
+
+  if (codigoDescuento === 'TIC10') {
+precio = precio * 0.90;
+  }
+
+  if (codigoDescuento === 'TIC20') {
+    precio = precio * 0.80;
+      }
+
+      if (codigoDescuento === 'DARIO') {
+        precio = precio * 0.50;
+          }
+
+  return precio;
+}
   let cantidadNum: number = Number(cantidad);
   let precio: number = calcularTotal(id, cantidadNum, codigoDescuento);
+  
   enviarAlFrontend('mostrarPrecio', precio);
+  
+  
 });
 
 iniciar();
